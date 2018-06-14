9815029028016a91553d3721e2d9a75b87f3cf9d
agpav <agrawalpavan@gmail.com>
2018-06-14 10:18:00 +0530

    Restructuring

--- src/main/java/org/ballbearing/context/Context.java
+++ src/main/java/org/ballbearing/context/Context.java
@@ -1,4 +1,4 @@
-package org.ballbearing.model;
+package org.ballbearing.context;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -7,6 +7,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-public @interface Model {
+public @interface Context {
 
 }
--- src/main/java/org/ballbearing/logic/Logic.java
+++ src/main/java/org/ballbearing/logic/Logic.java
@@ -1,13 +1,12 @@
 package org.ballbearing.logic;
 
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.CLASS;
-
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(CLASS)
-@Target(TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Logic {
 
 }
