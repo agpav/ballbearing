4f7a04b069a68afe20c6b9cafb597dac5bea808d
agpav <agrawalpavan@gmail.com>
2018-06-14 07:45:11 +0530

    First commit

---
+++ src/main/java/org/ballbearing/App.java
@@ -0,0 +1,13 @@
+package org.ballbearing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
